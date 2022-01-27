ed1b50123be1223c195c2974dd7d2f951d80bda2
thuyka <thuyka225@gmail.com>
2022-01-27 13:45:30 +0700

    02_Route

---
+++ 02_Route/task_manager/resources/views/modules/customer/detail.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+
+<body>
+    <h1>Danh sách khách hàng</h1>
+    <form action="" method="get"></form>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>STT</th>
+                <th>Họ và tên</th>
+                <th>Số điện thoại</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Nguyễn Văn A</td>
+                <td>01234567890</td>
+                <td>email.test@mail.com</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
---
+++ 02_Route/task_manager/resources/views/modules/customer/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Danh sách khách hàng</h1>
+    <form action="" method="get"></form>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>STT</th>
+                <th>Họ và tên</th>
+                <th>Số điện thoại</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><input type="text" placeholder="1"></td>
+                <td><input type="text" placeholder="Nguyễn Văn A"></td>
+                <td><input type="text" placeholder="01234567890"></td>
+                <td><input type="text" placeholder="email.test@mail.com"></td>
+                <td><button>update</button></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
